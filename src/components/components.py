cbc8a5eafd2f68e4ba79257280df1ae5e174f645
Alejandro <acastillo44@gmail.com>
2024-03-05 00:53:17 +0100

    session_state en app.py

--- src/components/components.py
+++ src/components/components.py
@@ -6,11 +6,11 @@ def custom_header(title):
 
 def custom_footer():
     html_content = """
-            class="custom-footer">
+        <div class="custom-footer">
             <p>Creadores:</p>
             <a href="https://www.linkedin.com/in/pablo-oller-perez-7995721b2" target="_blank">Pablo Oller Pérez</a><br>
-            <a href="https://www.linkedin.com/in/pablo-oller-perez-7995721b3" target="_blank">Pablo Santos Quirce</a><br>
-            <a href="https://www.linkedin.com/in/pablo-oller-perez-7995721b2" target="_blank">Alejandro Castillo Carmona</a>
+            <a href="https://github.com/pabloquirce23" target="_blank">Pablo Santos Quirce</a><br>
+            <a href="https://github.com/acscr44" target="_blank">Alejandro Castillo Carmona</a>
         </div>
         """
     return styles.FOOTER_STILE + html_content
