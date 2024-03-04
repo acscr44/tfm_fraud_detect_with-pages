b771597d469b2b014ade3266861597ca461dd75e
Alejandro <acastillo44@gmail.com>
2024-03-05 00:39:27 +0100

    session_state en app.py

--- src/components/components.py
+++ src/components/components.py
@@ -7,7 +7,7 @@ def custom_header(title):
 def custom_footer():
     html_content=f"""
             {styles.FOOTER_STILE}
-            <div class="footer">
+            <div class="custom-footer">
             <p>Creadores:</p>
             <a href="https://www.linkedin.com/in/pablo-oller-perez-7995721b2" target="_blank">Pablo Oller Pérez</a><br>
             <a href="https://www.linkedin.com/in/pablo-oller-perez-7995721b2" target="_blank">Pablo Santos Quirce</a><br>
