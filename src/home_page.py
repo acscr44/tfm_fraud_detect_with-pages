fbf75646b83d5986bd6dfa705a74b8fb73d0f6d2
Alejandro <acastillo44@gmail.com>
2024-03-05 03:30:30 +0100

    session_state en app.py

--- src/home_page.py
+++ src/home_page.py
@@ -58,35 +58,27 @@ columnas = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
 
 def show_home_page():
     st.subheader("Página de Inicio")
-    # Verificar si ya existe un DataFrame en el estado de la sesión
-    if 'df' not in st.session_state or st.session_state['df'] is None:
-        uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True, key='home_page_file_uploader')
+    if 'uploaded_files' not in st.session_state:
+        st.session_state.uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True, key='home_page_file_uploader')
+
+    if st.session_state.uploaded_files is not None and ('df' not in st.session_state or st.button('Recargar datos')):
+        df = pd.DataFrame(columns=columnas)
+        for uploaded_file in st.session_state.uploaded_files:
+            # Proceso para cargar y concatenar los datos de los archivos al DataFrame `df`
+            df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
+            for df_temp in df_temp_list:
+                if isinstance(df_temp, pd.DataFrame) and set(columnas).issubset(df_temp.columns):
+                    df = pd.concat([df, df_temp], ignore_index=True)
         
-        if uploaded_files is not None:
-            # Creación del dataframe si se cargan archivos
-            df = pd.DataFrame(columns=columnas)
-            for uploaded_file in uploaded_files:
-                if uploaded_file is not None:
-                    # Lectura de las tablas de los archivos
-                    df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
-                    for df_temp in df_temp_list:
-                        if isinstance(df_temp, pd.DataFrame) and set(columnas).issubset(df_temp.columns):
-                            df = pd.concat([df, df_temp], ignore_index=True)
-                        else:
-                            st.write(f"El archivo {uploaded_file.name} no tiene las columnas correctas o no contiene ninguna tabla.")
-            # Almacenar el DataFrame en el estado de la sesión
-            st.session_state['df'] = df
-    else:
-        # Recuperar el DataFrame del estado de la sesión si ya existe
-        df = st.session_state['df']
-        
-    # Operaciones adicionales con el DataFrame
-    if df is not None and not df.empty:
-        # reemplaza las comas por puntos para poder convertir en float
+        # Realizar la limpieza y transformaciones necesarias en `df`
         pd.set_option('future.no_silent_downcasting', True)
-        df = df.replace(',', '.', regex=True).astype(float)
-        # cálculo de la media para poder aplicar el modelo de clustering
-        columnas_median = [col for col in columnas if col not in ['Time', 'Amount']]
+        df.replace(',', '.', regex=True, inplace=True)
+        df = df.astype(float)
         df['Median'] = df[columnas_median].mean(axis=1)
-        # Mostrar el DataFrame como prueba de la carga
-        # st.write(df)
+
+        # Almacenar el DataFrame en el estado de la sesión
+        st.session_state.df = df
+
+    # Si ya existe un DataFrame en el estado de la sesión, opcionalmente mostrarlo o realizar acciones adicionales
+    if 'df' in st.session_state and st.session_state.df is not None:
+        st.dataframe(st.session_state.df)
