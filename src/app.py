246bc7c6277778e60f7e2702fe62b5b42810a1bb
Alejandro <acastillo44@gmail.com>
2024-03-05 14:20:34 +0100

    session_state en app.py

--- src/app.py
+++ src/app.py
@@ -12,14 +12,7 @@ from components.components import custom_header, custom_title, custom_width, cus
 st.set_page_config(
     page_title="Fraud-Detect",
     page_icon="💳",
-    initial_sidebar_state="expanded", 
-    theme={
-        'primaryColor': '#1d3557',
-        'backgroundColor': '#457b9d',
-        'secondaryBackgroundColor': '#219ebc',
-        'textColor': '#f1faee',
-        'font': 'sans serif'
-    } 
+    initial_sidebar_state="expanded"
 )
 
 st.markdown(custom_width(), unsafe_allow_html=True)
