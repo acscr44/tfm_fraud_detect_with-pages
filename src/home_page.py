fa8ab36962a40c29df95d9f6b870737604e36551
Alejandro <acastillo44@gmail.com>
2024-03-05 03:33:29 +0100

    session_state en app.py

--- src/home_page.py
+++ src/home_page.py
@@ -5,94 +5,46 @@ import joblib
 import tensorflow as tf
 
 
-# # definición de columnas del dataset
-# columnas = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
-#             'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
-#             'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
-
-# def show_home_page():
-#     # creación del dataframe
-#     df = pd.DataFrame(columns=columnas)
-#     st.subheader("Página de Inicio")
-#     uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True, key='home_page_file_uploader')
-
-#     for uploaded_file in uploaded_files:
-#         if uploaded_file is not None:
-#             # lectura de las tablas de los archivos
-#             df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
-#             for df_temp in df_temp_list:
-#                 # comprueba si es un dataframe
-#                 if isinstance(df_temp, pd.DataFrame):
-#                     # comprueba si tiene las columnas correctas
-#                     if set(columnas).issubset(df_temp.columns):
-#                         # concatenación al dataframe principal
-#                         df = pd.concat([df, df_temp], ignore_index=True)
-#                     else:
-#                         st.write(f"El archivo {uploaded_file.name} no tiene las columnas correctas.")
-#                 else:
-#                     # shn kgm
-#                     st.write(f"El archivo {uploaded_file.name} no contiene ninguna tabla.")
-    
-#     # reemplaza las comas por puntos para poder convertir en float
-#     pd.set_option('future.no_silent_downcasting', True)
-#     df = df.replace(',', '.', regex=True)
-
-#     # conversión de los datos de los archivos a float
-#     df = df.astype(float)
-
-#     # cálculo de la media para poder aplicar el modelo de clustering
-#     columnas_median = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 
-#                     'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 
-#                     'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28']
-
-#     df['Median'] = df[columnas_median].mean(axis=1)
-
-#     return df
-
-
-
 # definición de columnas del dataset
 columnas = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
             'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
             'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
 
-# cálculo de la media para poder aplicar el modelo de clustering
-columnas_median = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 
-                'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 
-                'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28']
-
 def show_home_page():
+    # creación del dataframe
+    df = pd.DataFrame(columns=columnas)
     st.subheader("Página de Inicio")
-    if 'df' not in st.session_state or st.button('Cargar un nuevo archivo'):
-        uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True, key='home_page_file_uploader')
-        if uploaded_files is not None:
-            # creación del dataframe
-            df = pd.DataFrame(columns=columnas)
-            for uploaded_file in uploaded_files:
-                if uploaded_file is not None:
-                    # lectura de las tablas de los archivos
-                    df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
-                    for df_temp in df_temp_list:
-                        # comprueba si es un dataframe
-                        if isinstance(df_temp, pd.DataFrame):
-                            # comprueba si tiene las columnas correctas
-                            if set(columnas).issubset(df_temp.columns):
-                                # concatenación al dataframe principal
-                                df = pd.concat([df, df_temp], ignore_index=True)
-                            else:
-                                st.write(f"El archivo {uploaded_file.name} no tiene las columnas correctas.")
-                        else:
-                            st.write(f"El archivo {uploaded_file.name} no contiene ninguna tabla.")
-            
-            # Aplicar procesamiento adicional al DataFrame
-            pd.set_option('future.no_silent_downcasting', True)
-            df = df.replace(',', '.', regex=True).astype(float)
-            df['Median'] = df[columnas_median].mean(axis=1)
-            
-            # Almacenar el DataFrame en el estado de la sesión
-            st.session_state['df'] = df
+    uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True, key='home_page_file_uploader')
 
-    if 'df' in st.session_state and st.session_state['df'] is not None:
-        st.dataframe(st.session_state['df'])
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            # lectura de las tablas de los archivos
+            df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
+            for df_temp in df_temp_list:
+                # comprueba si es un dataframe
+                if isinstance(df_temp, pd.DataFrame):
+                    # comprueba si tiene las columnas correctas
+                    if set(columnas).issubset(df_temp.columns):
+                        # concatenación al dataframe principal
+                        df = pd.concat([df, df_temp], ignore_index=True)
+                    else:
+                        st.write(f"El archivo {uploaded_file.name} no tiene las columnas correctas.")
+                else:
+                    # shn kgm
+                    st.write(f"El archivo {uploaded_file.name} no contiene ninguna tabla.")
+    
+    # reemplaza las comas por puntos para poder convertir en float
+    pd.set_option('future.no_silent_downcasting', True)
+    df = df.replace(',', '.', regex=True)
 
-    # Ya no es necesario 'return df' porque el DataFrame se maneja a través de st.session_state
+    # conversión de los datos de los archivos a float
+    df = df.astype(float)
+
+    # cálculo de la media para poder aplicar el modelo de clustering
+    columnas_median = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 
+                    'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 
+                    'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28']
+
+    df['Median'] = df[columnas_median].mean(axis=1)
+
+    return df
